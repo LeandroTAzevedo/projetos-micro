5759f6cbd86dad14a87bf471543e0f6e5a2f63f2
Leandro Azevedo <leandrotostes2009@usp.br>
2023-07-01 20:49:04 -0300

    final

--- pratica3.c
+++ pratica3.c
@@ -1,93 +1,79 @@
-//pratica 3 - aplicacao de microprocessadores
+// Pratica 3 - aplicacao de microprocessadores
 
-// pinos utilizados para comunica??o com o display LCD
-sbit LCD_RS at LATB4_bit; // pino 4 do PORTB interligado ao RS do display
-sbit LCD_EN at LATB5_bit; // pino 5 do PORTB " " ao EN do display
-sbit LCD_D4 at LATB0_bit; // pino 0 do PORTB ao D4
-sbit LCD_D5 at LATB1_bit;  // " "
-sbit LCD_D6 at LATB2_bit;  // " "
-sbit LCD_D7 at LATB3_bit;  // " "
+// Configuracao de pinos do LCD (PORTB)
+// Pinos utilizados para comunicacao com o display LCD
+sbit LCD_RS at LATB4_bit; // Pino 4 do PORTB interligado ao RS do display
+sbit LCD_EN at LATB5_bit; // Pino 5 do PORTB interligado ao EN do display
+sbit LCD_D4 at LATB0_bit; // Pino 0 do PORTB interligado ao D4 do display
+sbit LCD_D5 at LATB1_bit; // Pino 1 do PORTB interligado ao D5 do display
+sbit LCD_D6 at LATB2_bit; // Pino 2 do PORTB interligado ao D6 do display
+sbit LCD_D7 at LATB3_bit; // Pino 3 do PORTB interligado ao D7 do display
+// Direcao do fluxo de dados nos pinos selecionados
+sbit LCD_RS_Direction at TRISB4_bit; // Direcao do fluxo de dados do pino RB4
+sbit LCD_EN_Direction at TRISB5_bit; // Direcao do fluxo de dados do pino RB5
+sbit LCD_D4_Direction at TRISB0_bit; // Direcao do fluxo de dados do pino RB0
+sbit LCD_D5_Direction at TRISB1_bit; // Direcao do fluxo de dados do pino RB1
+sbit LCD_D6_Direction at TRISB2_bit; // Direcao do fluxo de dados do pino RB2
+sbit LCD_D7_Direction at TRISB3_bit; // Direcao do fluxo de dados do pino RB3
 
-// dire??o do fluxo de dados nos pinos selecionados
-sbit LCD_RS_Direction at TRISB4_bit;  // dire??o do fluxo de dados do pino RB4
-sbit LCD_EN_Direction at TRISB5_bit;  // " "
-sbit LCD_D4_Direction at TRISB0_bit;
-sbit LCD_D5_Direction at TRISB1_bit;
-sbit LCD_D6_Direction at TRISB2_bit;
-sbit LCD_D7_Direction at TRISB3_bit;
+ unsigned char Tensao[10]; // Display LCD - tipo char - int 8 bits
+ char Temp[8];
+ int Va;
+ int Ta;
+ int Vmax = 500;
+ int Tmax = 999;
 
-char Tensao[6]; // Display LCD - tipo char - int 8 bits
-char Temp[8];    // mudar isso dps
-int Va;
-int Ta;
-int Vmax = 500;
-int Tmax = 999;
+// Inicio do programa
+void main() {
 
-void main(){
+ // Configuracao inicial de pinos, entrada, saida e registradores ADC
+ TRISA.RA0 = 1; // Pino RA0 como entrada (canal analogico AN0)
+ TRISE.RE1 = 1; // Pino RE1 como entrada (canal analogico AN6)
+ //PORTA.RA0 = 1; // opcional
+ ADCON0 = 0B00000001; // AN0 -> AD ligado, leitura deslig., canal AN0
+ ADCON2 = 0B10101010; // Justificativa para direita, FOSC/32 (tempo entre 2 e 25 us)
+ //... e 12 TAD (tempo de conversao de cada bit + 2 TAD)
+ #ifdef P18F45K22 // Utilizando um PIC18F45k22
+ ANSELA.B0 = 1; // Somente bit 0 ("B0") do PORTA sera usado (ou seja: RA0/AN0) como analogico
+ ANSELB = 0; // PORTB = 0, isto e, como I/O digital,  pois nao sera usado como analogico
+ ADCON1 = 0B00000000; // tensoes de ref. internas = VDD e Vss
+ VREFCON0 = 0B00010000; // Vref+ interna ajustada para 1,024V
+ #endif
+ #ifdef P18F4550 // Utilizando um PIC18F4550
+ ADCON1=0B00001000; // Tensao de ref interna, canais AN6-AN0 como analogico
+ #endif
 
-// Configuracao inicial (pinos, entrada, saida e registradores ADC)
-TRISA.RA0 = 1; // Pino RA0 como entrada (canal analogico AN0)
-TRISE.RE1 = 0; // Pino RE1 como entrada (canal analogico AN6)
-//PORTA.RA0 = 1; // opcional
-ADCON0 = 0B00000001; // AN0 -> AD ligado, leitura deslig., canal AN0
-ADCON2 = 0B10101010; // Justificativa para direita, FOSC/32 (tempo entre 2 e 25 us)
-//... e 12 TAD (tempo de conversao de cada bit + 2 TAD)
-#ifdef P18F45K22 // Utilizando um PIC18F45k22
-ANSELA.B0 = 1; // Somente bit 0 ("B0") do PORTA sera usado (ou seja: RA0/AN0) como analogico
-ANSELB = 0; // PORTB = 0, isto e, como I/O digital,  pois nao sera usado como analogico
-ADCON1 = 0B00000000; // tensoes de ref. internas = VDD e Vss
-VREFCON0 = 0B00010000; // Vref+ interna ajustada para 1,024V
-TRISA.RA2 = 1;
-TRISA.RA3 = 1;
-#endif
-
-#ifdef P18F4550 // Utilizando um PIC18F4550
-ADCON1 = 00011011;   //ref+ = an3, vref- = 0, an0 - an3 = analogico, resto digital.
-TRISA.RA1 = 1;
-TRISA.RA2 = 1;
-TRISA.RA3 = 1;
-#endif
-
-ADC_init();
-
-
-
-
-Lcd_Init();                        //Inicializa display no modo 4 bits
-Lcd_Cmd(_LCD_CLEAR);               //Apaga display
-Lcd_Cmd(_LCD_CURSOR_OFF);
-
-//Lcd_Out(1,6,"    V       ");
-//Lcd_Out(2,7,"    C       ");
-
-while(1){
-  //LCD_CMD(_LCD_SECOND_ROW);     //testes com cursor
+ // Configuracao do modulo ADC
+ ADC_init();
+ // Configuracao do modulo LCD
+ Lcd_Init(); // Inicializa o display no modo 4 bits
+ Lcd_Cmd(_LCD_CLEAR); // Apago o display
+ Lcd_Cmd(_LCD_CURSOR_OFF); // Desliga o cursor
+ Lcd_Out(1,1,"Tensao:       V "); // Escrita no LCD
+ Lcd_Out(2,1," Temp.:        oC"); // Escrita no LCD
+ 
+ while(1) {
+ 
+  // Valor de tensao
   Va = ADC_Read(0);
   Va = Va * (Vmax/1023.);
   Tensao[0] = (Va/100)%10 + '0';
   Tensao[1] = '.';
   Tensao[2] = (Va/10)%10 + '0';
   Tensao[3] = (Va/1)%10 + '0';
-  //Lcd_Out_Cp(Tensao);     //testes com cursor
-  //Lcd_Out_Cp("V");
-  Lcd_Out(1,1,Tensao); // Mostra os valores no display
+  LCD_Out(1,9,Tensao); // Apresenta o texto no LCD em determinada posicao
   Delay_ms(20); // Delay para permitir a atualizacao do LCD
-
-
-//temp:
-
-//Lcd_Cmd(_LCD_first_ROW);
-Ta = ADC_Read(1);
-Ta = Ta * (Tmax/1023.);
-Temp[0] = (Ta/100)%10 + '0';
-Temp[1] = (Ta/10)%10 + '0';
-Temp[2] = '.';
-Temp[3] = (Ta)%10 + '0';
-Temp[4] = 0;
-//Lcd_Out_Cp(Temp);     //testes com cursor
-//Lcd_Out_Cp("C");
-Lcd_Out(2,1, Temp);
-Delay_ms(20);
-//Lcd_Cmd(_LCD_CLEAR);
-  }
+  
+  // Valor de temperatura
+  Ta = ADC_Read(6);
+  Ta = Ta * (Tmax/1023.);
+  Temp[0] = (Ta/100)%10 + '0';
+  Temp[1] = (Ta/10)%10 + '0';
+  Temp[2] = '.';
+  Temp[3] = (Ta)%10 + '0';
+  Temp[4] = 0;
+  Lcd_Out(2,9,Temp); // Apresenta o texto no LCD em determinada posicao
+  Delay_ms(20); // Delay para permitir a atualizacao do LCD
+  
+ } // Fim de "while(1)"
 } // Fim de "void main()"
